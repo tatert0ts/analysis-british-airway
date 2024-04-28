357051ee955a3e5d1e231937b30ed3db521f084a
tatert0ts <tanjiayin.work@gmail.com>
2024-04-28 19:52:48 +0800

    clean up

--- bair_dash.py
+++ bair_dash.py
@@ -1,54 +1,23 @@
 import pandas as pd
 import plotly.express  as px
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html, dash_table
+# from dash import html
+import numpy as np
 from dash.dependencies import Output, Input
 import dash_bootstrap_components as dbc
+from scripts.sentiments import *
 
-from wordcloud import WordCloud, STOPWORDS
-import string
-
-from test.sentiments import fig_word_tm
-                # html.Div(dcc.Graph(figure = fig_word_tm)),
-
-# to do: verified vs rating correlation?
-
-df = pd.read_csv(r'C:\Users\niyai\git\british-analysis\British_Airway_Review_cleaned.csv')
+df = pd.read_csv(r'C:\Users\niyai\git\british-analysis\data\British_Airway_Review_cleaned.csv')
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-# app.title = "slay"
+app.title = "British Airway Analysis"
 server = app.server
-
-# colors = {'coolblack' : '#01295C',
-#           'persianblue' : '#075AAA',
-#           'beaublue' : '#B9CFED',
-#           'pigmentred' : '#EB2226',
-#           'metallicsilver' : '#A7A9AC',
-#           'notwhite' : '#EFE9E5'
-#           }
-
-# colors.get('coolblack')
-# https://www.schemecolor.com/british-airways-website.php
-
-text = " ".join(review for review in df.cleaned_reviews)
-text = text.translate(str.maketrans('', '', string.punctuation))
-stop_words = set(STOPWORDS)
-stop_words.update(['ba', 'flight', 'flights', 'british', 'airway', 'airways'])
-# Tokenize the text into words and remove empty strings
-cleaned_words = [word for word in text.split() if word.strip()]
-# convert into lowercase
-# checking words against stopwords
-cleaned_words = [word for word in cleaned_words if word.lower() not in stop_words]
-word_freq = pd.Series(cleaned_words).value_counts()
-df_word_freq = word_freq.reset_index()
-df_word_freq.columns = ['Word', 'Frequency']
-# Plot the treemap
-fig_wordcloud = px.treemap(df_word_freq.head(50), path=['Word'], values='Frequency', title='Most Frequent Words')
 
 app.layout = html.Div([
     html.Div(
+##################################################### header
             id = "header-area",
             children=[
                 html.P(),
@@ -69,9 +38,10 @@ app.layout = html.Div([
         ),
     html.P(),
     dcc.Tabs([
+##################################################### first tab
         dcc.Tab(label = 'Analytics', 
     children=[
-        ##################################################### filter 
+##################################################### filter 
         html.P(),
         html.Div(
             dbc.Row(
@@ -114,7 +84,7 @@ app.layout = html.Div([
                         )])
                         , width=2),
                 dbc.Col(html.Div([
-                   html.P(id = 'filter-recc-name', children = ('Recommended')),
+                   html.P(id = 'filter-recc-name', children = ('Recommend')),
                     dcc.Checklist(
                         id = "filter-recc",
                         options = [{"label": recommended, "value": recommended} for recommended in df.recommended.unique()],
@@ -126,19 +96,16 @@ app.layout = html.Div([
                         'font-size': '13px'
                         }
         )),
-        ##################################################### visuals 
+##################################################### organising visuals 
         html.P(),
         html.Div(
             id = "graph-container",
             children = 
-            [   dbc.Row(dbc.Col(html.Div(dcc.Graph(id = "chart-reviews")), width = {'size': 10, 'offset': 1})),
-                html.P(),
-                # html.P("Review Ratings:", style = {'textAlign' : 'center','font-size': '18px', 'color' : 'white'}),
-                # html.P(id = 'para-stars', style = {'textAlign' : 'center','font-size': '16px', 'color' : 'white'}),
-                # dbc.Row(
-                #     [dbc.Col(html.Div(dcc.Graph(id = "chart-stars")), width = {'size': 6, 'offset': 1}),
-                #     dbc.Col(html.Div(dcc.Graph(id = "chart-stars-rec")), width = 4),
-                #     ]),
+            [   dbc.Row(
+                        [dbc.Col(html.Div(dcc.Graph(id = "chart-reviews")), width = {'size': 5, 'offset': 1}),
+                        dbc.Col(html.Div(dcc.Graph(id = "chart-recc")), width = {'size': 5})
+                        ]),
+                html.P()
             ]),
         html.P(),
         html.Div(
@@ -146,44 +113,30 @@ app.layout = html.Div([
             children = 
             [   html.P("Distribution of Reviewers:", style = {'textAlign' : 'center','font-size': '18px', 'color' : 'white'}),
                 dbc.Row(
-                    [dbc.Col(html.Div(dcc.Graph(id = "seattype-chart")), width = {'size': 5, 'offset': 1}),
-                    dbc.Col(html.Div(dcc.Graph(id = "traveller-chart")), width = 5),
+                    [dbc.Col(html.Div(dcc.Graph(id = "chart-seattype")), width = {'size': 5, 'offset': 1}),
+                    dbc.Col(html.Div(dcc.Graph(id = "chart-traveller")), width = 5),
                     ]),
                 html.P(),
-                # html.P("Demographics of Reviewers:", style = {'textAlign' : 'center','font-size': '18px', 'color' : 'white'}),
                 dbc.Row(
-                    [dbc.Col(html.Div(dcc.Graph(id = "country-chart")), width = {'size': 6, 'offset': 1}),
-                    dbc.Col(html.Div(dcc.Graph(id = "route-chart")), width = 4),
+                    [dbc.Col(html.Div(dcc.Graph(id = "chart-country")), width = {'size': 6, 'offset': 1}),
+                    dbc.Col(html.Div(dcc.Graph(id = "chart-route")), width = 4),
                     ]),
+                html.P(id = 'comment-nationality', style = {'textAlign' : 'center' ,'font-size': '14px', 'color' : 'white'}),
             ]
         )
     ]
-    ), # end of tab 1 
-    dcc.Tab(label = 'Insights', 
-            children = [
-                html.P(children = [
-                    '1. fig_rev_num - to note that there is a decreasing number of reviews as time passes, can attribute to datasets\' lack of upkeep - highest reviews in 2017, lowest in 2021 kiv when drawing conclusions from the rest of the dashboard.',
-                       html.Br(), 
-                       '3. fig_seattype - majority of the reviewers are from economy class, what is the rough % of seats available by seat type? does the proportion correspond?, hover tip to include number of customers, change the colour scheme',
-                       html.P(),
-                       '4. fig_travellertype - majority leisure, LEISURE vs BUSINESS -- are there any significant differences in ratings and sentiments???, hover tip to include number of customers, change the colour scheme',
-                       html.P(),
-                       '5. fig_country - countries with less thn 12.5 reviewers are categorised together under others, there are 47 unique countries, change details in the hover tip- consider top 10 only',
-                       html.P(),
-                       '7. fig_route - inconsistency with shortform and longhand'
-                ], style = {'textAlign' : 'left','font-size': '14px', 'color' : 'white'}
-                )
-        ]),
-    dcc.Tab(label = 'WordCloud', 
-            children = [
-                dbc.Col(html.Div(dcc.Graph(id = "chart-wordreviews", figure = fig_wordcloud)), width = {'size': 10, 'offset': 1})
+    ), 
+    dcc.Tab(
+##################################################### second tab 
+        label = 'WordCloud', 
+        children = [
+            html.P(),
+            dbc.Row([
+                dbc.Col(html.Div(dcc.Graph(id = "chart-treemap", figure = fig_rw)), width = {'size': 10, 'offset': 1})
+                ])
             ]),
     ])
 ])
-
-
-
-
 
 def filter_data (start_date, end_date, seattype, traveller, verified, recc):
     df1 = df.loc[(df.date >= start_date) & (df.date <= end_date)] # date filter
@@ -193,8 +146,16 @@ def filter_data (start_date, end_date, seattype, traveller, verified, recc):
     df5 = df4[df4['recommended'].isin(recc)] # recommended type filter
     return df5
 
+##################################################### callbacks
 @app.callback(
+    [Output("chart-recc", "figure"),
     Output("chart-reviews", "figure"),
+    Output("chart-seattype", "figure"),
+    Output("chart-traveller", "figure"),
+    Output("chart-country", "figure"),
+    Output("chart-route", "figure"),
+    Output("comment-nationality", "children")
+    ],
     [Input("filter-date", "start_date"),
     Input("filter-date", "end_date"),
     Input("filter-seattype", "value"),
@@ -203,8 +164,27 @@ def filter_data (start_date, end_date, seattype, traveller, verified, recc):
     Input("filter-recc", "value")
     ]
 )
-def update_graph (start_date, end_date, seattype, traveller, verified, recc):
+def update (start_date, end_date, seattype, traveller, verified, recc):
     df_cleaned = filter_data(start_date, end_date, seattype, traveller, verified, recc)
+
+    ######################### chart-recc
+    recc = df_cleaned.groupby(['ym', 'recommended']).size().reset_index(name='count')
+    fig_recc = px.line(recc, x = "ym", y = "count",
+                       color = 'recommended',
+                       color_discrete_map = {'No': 'red', 'Yes': 'green'}
+                        )
+    fig_recc.update_layout(hovermode="x",
+                        title = "Recommendation Overtime",
+                        title_x = 0.5,
+                        yaxis_title = 'Number of Reviews',
+                        xaxis_title = 'Date',
+                        legend_title = None,
+                        legend = dict(yanchor = "top", y = 0.98,
+                                xanchor = "right", x = 0.99),
+                        margin = dict(l = 50, r = 40, t = 50, b = 30)
+                        )
+    
+    ######################### chart-reviews
     rev_num = df_cleaned.groupby(['ym', 'seat_type']).size().reset_index(name='count')
     fig_rev_num = px.bar(rev_num, x = "ym", y = "count",
                         color = 'seat_type',
@@ -221,100 +201,68 @@ def update_graph (start_date, end_date, seattype, traveller, verified, recc):
                               xanchor = "right", x = 0.99),
                     margin = dict(l = 50, r = 40, t = 50, b = 30)
                     )
-    return fig_rev_num
-
-@app.callback(
-    Output("seattype-chart", "figure"),
-    [Input("filter-date", "start_date"),
-    Input("filter-date", "end_date"),
-    Input("filter-seattype", "value"),
-    Input("filter-traveller", "value"),
-    Input("filter-verified", "value"),
-    Input("filter-recc", "value")
-    ]
-)
-def update_graph (start_date, end_date, seattype, traveller, verified, recc):
-    df_cleaned = filter_data(start_date, end_date, seattype, traveller, verified, recc)
-    fig_seattype = px.pie(df_cleaned, names = "seat_type", color = "seat_type",
-                color_discrete_map = {'Business Class': '#075AAA', 'Economy Class': '#A7A9AC',
-                                      'First Class': '#B9CFED', 'Premium Economy' : '#01295C' })
+    
+    ######################### chart-seattype
+    fig_seattype = px.pie(df_cleaned, 
+                        names = "seat_type", 
+                        color = "seat_type",
+                        color_discrete_map = {'Business Class': '#075AAA', 'Economy Class': '#A7A9AC',
+                                            'First Class': '#B9CFED', 'Premium Economy' : '#01295C' })
     fig_seattype.update_layout(title = "By Seat Type", 
                                title_x = 0.5,
                                margin = dict(l = 50, r = 40, t = 50, b = 30)
                                )
-    return fig_seattype
-
-@app.callback(
-    Output("traveller-chart", "figure"),
-    [Input("filter-date", "start_date"),
-    Input("filter-date", "end_date"),
-    Input("filter-seattype", "value"),
-    Input("filter-traveller", "value"),
-    Input("filter-verified", "value"),
-    Input("filter-recc", "value")
-    ]
-)
-def update_graph (start_date, end_date, seattype, traveller, verified, recc):
-    df_cleaned = filter_data(start_date, end_date, seattype, traveller, verified, recc)
-    fig_travellertype = px.pie(df_cleaned, names = "type_of_traveller", color = 'type_of_traveller',
-                            color_discrete_sequence = px.colors.sequential.RdBu
+    
+    ######################### chart-traveller
+    # exploring if able to present in sunburst, where we can visualise % of yes and no in each category
+    # fig_travellertype = px.sunburst(df_cleaned, 
+                                # path = ['type_of_traveller','recommended'],
+                                # color = 'recommended', 
+                                # color_discrete_map = {'yes': 'green', 'no': 'red', '(?)' : 'royal blue'
+                                #                     }
+    fig_travellertype = px.pie(df_cleaned, 
+                                names = 'type_of_traveller',
+                                color = 'type_of_traveller',
+                                color_discrete_map = 
+                                    {'Couple Leisure': '#075AAA', 'Family Leisure': '#A7A9AC',
+                                    'Solo Leisure': '#B9CFED', 'Business' : '#01295C' }
                             )
     fig_travellertype.update_layout(
                     title = "By Traveller Purpose",
                     title_x = 0.5,
                     margin = dict(l = 50, r = 40, t = 50, b = 30))
-    return fig_travellertype
+    
+    ######################### chart-country
+    df_country = df_cleaned.groupby(['country']).size().reset_index(name='count')
+    df_c_index = df_country['count'].nlargest(10)
+    top10_counts = df_country[df_country.index.isin(df_c_index.index)]
+    fig_country = px.bar(top10_counts, x = 'country', y = 'count',
+                            color = 'country',
+                            color_discrete_sequence = px.colors.sequential.ice,
+                            text_auto = True)
+    fig_country.update_layout(hovermode="x",
+                            title = "By Top 10 Countries",
+                            title_x = 0.5,
+                            yaxis_title = 'Number of Reviews',
+                            xaxis_title = None,
+                            xaxis = {'categoryorder':'total descending'},
+                            showlegend = False,
+                            margin = dict(l = 50, r = 40, t = 50, b = 30)
+                            )
+    fig_country.update_traces(textposition="outside")
 
-
-# len(min_cat.unique())
-
-@app.callback(
-    Output("country-chart", "figure"),
-    [Input("filter-date", "start_date"),
-    Input("filter-date", "end_date"),
-    Input("filter-seattype", "value"),
-    Input("filter-traveller", "value"),
-    Input("filter-verified", "value"),
-    Input("filter-recc", "value")
-    ]
-)
-def update_graph (start_date, end_date, seattype, traveller, verified, recc):
-    df_cleaned = filter_data(start_date, end_date, seattype, traveller, verified, recc)
-    # Define the threshold for minority categories
-    threshold = 2500*0.005 # bottom 0.5% anomalies
-    # Count the frequency of each category
-    country_counts = df_cleaned['country'].value_counts()
-    # Identify categories below the threshold
-    min_cat = country_counts[country_counts < threshold].index
-    # Replace minority categories with 'Others'
-    df_cleaned['new_country'] = df_cleaned['country'].where(~df_cleaned['country'].isin(min_cat), 'Others')
-    fig_country = px.treemap(df_cleaned, path = ["new_country"], color = 'new_country', color_discrete_sequence = px.colors.sequential.RdBu
-                             )
-    fig_country.update_xaxes(categoryorder='total descending')
-    fig_country.update_layout(
-                    title = "By Nationality", title_x = 0.5,
-                    margin = dict(l = 50, r = 40, t = 50, b = 30)
-                    )
-    return fig_country
-
-@app.callback(
-    Output("route-chart", "figure"),
-    [Input("filter-date", "start_date"),
-    Input("filter-date", "end_date"),
-    Input("filter-seattype", "value"),
-    Input("filter-traveller", "value"),
-    Input("filter-verified", "value"),
-    Input("filter-recc", "value")
-    ]
-)
-def update_graph (start_date, end_date, seattype, traveller, verified, recc):
-    df_cleaned = filter_data(start_date, end_date, seattype, traveller, verified, recc)
+    ######################### comment-nationality
+    not_top10_counts = df_country[~df_country.index.isin(df_c_index.index)]
+    
+    ######################### chart-route
     fig_route = px.sunburst(df_cleaned, path = ['from','to'], color_discrete_sequence = px.colors.sequential.RdBu)
     fig_route.update_layout(title = "By Travel Routes",
                             title_x = 0.5,
                             margin = dict(l = 50, r = 40, t = 50, b = 30)
                             )
-    return fig_route
+    fig_route.update_traces(textinfo = "label+percent parent")
+
+    return fig_recc, fig_rev_num, fig_seattype, fig_travellertype, fig_country, fig_route, "there is a total of {} nationalities which are not in the top 10 and amounted to a count of {}".format(len(not_top10_counts.country.unique()), not_top10_counts['count'].sum())
 
 if __name__ == "__main__":
     app.run_server(debug=True)
---
+++ scripts/sentiments.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import string
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r'C:\Users\niyai\git\british-analysis\data\British_Airway_Review_cleaned.csv')
+
+stop_words = set(STOPWORDS)
+stop_words.update(['ba', 'flight', 'flights', 'british', 'airway', 'airways'])
+
+def extractwords(df):
+    # into a single string text, with each review separated by a space
+    text = " ".join(review for review in df.cleaned_reviews)
+    # remove all punctuation characters from the text
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # removing any extra whitespace and ensuring that the list only contains non-empty words
+    cleaned_words = [word for word in text.split() if word.strip()]
+    # convert into lowercase
+    # checking words against stopwords
+    cleaned_words = [word.lower() for word in cleaned_words if word.lower() not in stop_words]
+    word_freq = pd.Series(cleaned_words).value_counts()
+    df_word_freq = word_freq.reset_index()
+    df_word_freq.columns = ['Word', 'Frequency']
+    return df_word_freq
+
+df_yes = pd.DataFrame(df[df['recommended'] == 'Yes']['cleaned_reviews'])
+df_no = pd.DataFrame(df[df['recommended'] == 'No']['cleaned_reviews'])
+
+df_concatenated = pd.concat([extractwords(df_yes), extractwords(df_no)], keys=['Yes', 'No'])
+df_concatenated['Reccomended'] = df_concatenated.index.get_level_values(0)
+df_concatenated = df_concatenated.reset_index(drop=True) # remove keys
+
+topwords = extractwords(df).head(50).Word 
+df_topwords = df_concatenated[df_concatenated['Word'].isin(topwords.values)]
+
+# Plot the treemap
+fig_wr = px.treemap(df_topwords, path=['Word', 'Reccomended']
+                 , values='Frequency', title='Most Frequent Words')
+fig_wr.update_layout(title_x = 0.5, 
+                  margin = dict(l = 50, r = 40, t = 50, b = 30)
+                  )
+
+# Plot the treemap
+fig_rw = px.treemap(df_topwords, path=['Reccomended','Word']
+                 , values='Frequency', title='Most Frequent Words',
+                 color = 'Reccomended',
+                 color_discrete_map={'No':'red', 'Yes':'green'})
+fig_rw.update_layout(title_x = 0.5, 
+                  margin = dict(l = 50, r = 40, t = 50, b = 30)
+                  )
+
+
+# word cloud
+# fig_wc = WordCloud(background_color='white',max_words=100,max_font_size=300,width=1600,height=800, stopwords=stop_words)
+# fig_wc.generate(" ".join(cleaned_words))
+# plt.imshow(fig_wc)
+# plt.tight_layout()
+# plt.axis('off')
